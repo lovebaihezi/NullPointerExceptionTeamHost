df2a94b66380664524aa8f4104cce768edcc5aa4
lovebaihezi <x18739168862@gmail.com>
2020-12-21 19:26:50 +0800

    add react router for project link

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from './NullPointerException.svg';
+import ProjectLink from "./pages/Our-project";
 import './App.css';
 
 const App: React.FC<any> = () => {
     return (
         < Router>
-            <div className="App">
+            < div className="App">
                 < header className="App-header">
                     < img src={logo} className="App-logo" alt="logo" />
                     < p>
                         Welcome to the <code><strong>NullPointerException</strong></code> team place
                     </p>
-                    <Link to="Projects">
+                    < Link to="Projects">
                         <code><strong>Link : Our Team Projects</strong></code>
                     </Link>
-                    <Link to="AboutUs">
+                    < Link to="AboutUs">
                         <code><strong>Link : About Us</strong></code>
                     </Link>
                 </header>
             </div>
             < Switch>
+                < Route exact path="/Projects">
+                    <ProjectLink />
+                </Route>
+                < Route path="/AboutUs">
+
+                </Route>
             </Switch>
         </Router>
     );
---
+++ src/pages/Our-project.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ProjectLink: React.FC<any> = (props) => < div>
+    < a href="http://nulllpointerexception.work">
+        <code><strong>smart-meeting</strong></code>
+    </a>
+</div>
+
+export default ProjectLink
